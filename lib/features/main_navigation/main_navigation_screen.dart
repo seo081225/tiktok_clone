1324626cdf8e0c7bb291a910c35c26c366d21092
seo <seo081225@gmail.com>
2023-09-03 17:27:46 +0900

    #12.0 Introduction

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -6,6 +6,7 @@ import 'package:tiktok_clone_2023/features/discover/discover_screen.dart';
 import 'package:tiktok_clone_2023/features/inbox/inbox_screen.dart';
 import 'package:tiktok_clone_2023/features/main_navigation/widgets/nav_tab.dart';
 import 'package:tiktok_clone_2023/features/main_navigation/widgets/post_video_button.dart';
+import 'package:tiktok_clone_2023/features/users/user_profile_screen.dart';
 import 'package:tiktok_clone_2023/features/videos/video_timeline_screen.dart';
 
 class MainNavigationScreen extends StatefulWidget {
@@ -16,7 +17,7 @@ class MainNavigationScreen extends StatefulWidget {
 }
 
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
-  int _selectedIndex = 3;
+  int _selectedIndex = 4;
 
   void _onTap(int index) {
     setState(() {
@@ -56,7 +57,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
           ),
           Offstage(
             offstage: _selectedIndex != 4,
-            child: Container(),
+            child: const UserProfileScreen(),
           )
         ],
       ),
