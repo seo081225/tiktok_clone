6b7027cb33b5566d13b82e88fda223e228ebe5d3
seo <seo081225@gmail.com>
2023-09-18 01:11:52 +0900

    #25.3 createProfile

--- lib/features/authentication/repos/authentication_repo.dart
+++ lib/features/authentication/repos/authentication_repo.dart
@@ -10,7 +10,7 @@ class AuthenticationRepository {
   Stream<User?> authStateChanges() => _firebaseAuth.authStateChanges();
 
   Future<UserCredential> emailSignUp(String email, String password) async {
-    await _firebaseAuth.createUserWithEmailAndPassword(
+    return _firebaseAuth.createUserWithEmailAndPassword(
       email: email,
       password: password,
     );
